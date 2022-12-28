84e0185d313f6ae848e852d3d958a54ae17538a4
FarrukhAdeel67 <m.farrukhadeel@gmail.com>
2022-12-28 13:48:07 +0500

    upto reusing custom hooks

--- frk-blog-backend/routes/blog.js
+++ frk-blog-backend/routes/blog.js
@@ -2,5 +2,6 @@ const { Router } = require("express");
 const router = Router();
 const controller = require("../controllers/blog")
 router.get('/', controller.get);
+router.get("/:id", controller.getBlog);
 router.post('/createBlog', controller.createBlog);
 module.exports = router;
--- frk-blog-frontend/src/Home.js
+++ frk-blog-frontend/src/Home.js
@@ -1,32 +1,12 @@
-import {useState, useEffect} from 'react'
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending]= useState(true) ;
-    const [error, setError] = useState(null);
-    useEffect(()=>{
-        fetch('http://localhost:4000/api/blogs')
-        .then((res)=>{
-            if(!res.ok){
-                throw Error("Couldn't Fetch the data from that Resource");
-            }
-            return res.json();
-        })
-        .then(data =>{
-            setBlogs(data);
-            setIsPending(false);
-            setError(null);
-        })
-        .catch(err=>{
-            setError(err.message);
-            setIsPending(false);
-        });
-    },[])
+   const {data: blogs, isPending, error} = useFetch('http://localhost:4000/api/blogs')
     return ( 
         <div className="home">
             {error && <div>{error}</div>}
             {isPending && <div>Loading....</div>}
-           { blogs &&<BlogList blogs = {blogs} title= "All Blogs"/> }
+           { blogs && <BlogList blogs = {blogs} title= "All Blogs"/> }
         </div>
      );
 }
